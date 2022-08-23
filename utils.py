7d93d7b801603ec48747a1658d910c50dcc82fb8
Omri <110406612+OmriPy@users.noreply.github.com>
2022-08-23 18:24:21 +0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,39 @@
+import pygame
+import os
+
+def quit():
+    pygame.quit()
+    pygame.font.quit()
+    pygame.display.quit()
+
+def join(dirs: list[str]) -> str:
+    path = dirs[0]
+    for i in range(len(dirs) - 1):
+        path = os.path.join(path, dirs[i+1])
+    return path
+
+def equal(vels: list[int]) -> bool:
+    for i in range(len(vels)):
+        if vels[i] != vels[0]: return False
+    return True
+
+def Transition(screen: pygame.Surface ,starting_color: tuple[int, int, int] | list[int, int, int], ending_color: tuple[int, int, int] | list[int, int, int]):
+    starting_color = list(starting_color)
+    ending_color = list(ending_color)
+    current = starting_color
+    while current != [0, 0, 0]:
+        for i in range(len(current)):
+            if current[i] > 0:
+                current[i] -= 1
+                screen.fill(tuple(current))
+                pygame.display.update()
+                #pygame.time.delay(2)
+            else: continue
+    while current != ending_color:
+        for i in range(len(current)):
+            if current[i] < ending_color[i]:
+                current[i] += 1
+                screen.fill(tuple(current))
+                pygame.display.update()
+                #pygame.time.delay(2)
+            else: continue

12f9ae116a341505de8d3d5a41a2a6182511b68c
Omri <110406612+OmriPy@users.noreply.github.com>
2022-09-08 18:11:00 +0300

    Update utils.py

--- Windows/utils.py
+++ Windows/utils.py
@@ -1,9 +1,14 @@
 import pygame
 import os
 
+def quit():
+    pygame.display.quit()
+    pygame.font.quit()
+    pygame.quit()
+
 def join(dirs: list[str]) -> str:
     path = dirs[0]
-    for i in range(len(dirs)-1):
+    for i in range(len(dirs) - 1):
         path = os.path.join(path, dirs[i+1])
     return path
 
@@ -12,28 +17,19 @@ def equal(vels: list[int]) -> bool:
         if vels[i] != vels[0]: return False
     return True
 
-def Transition(screen: pygame.Surface ,starting_color: tuple[int, int, int] | list[int, int, int], ending_color: tuple[int, int, int] | list[int, int, int]):
+def Transition(screen: pygame.Surface, starting_color: tuple | list, ending_color: tuple | list):
     starting_color = list(starting_color)
     ending_color = list(ending_color)
     current = starting_color
     while current != [0, 0, 0]:
+        screen.fill(tuple(current))
+        pygame.display.update()
         for i in range(len(current)):
             if current[i] > 0:
                 current[i] -= 1
-            else: continue
+    while current != ending_color:
         screen.fill(tuple(current))
         pygame.display.update()
-        pygame.time.delay(2)
-    while current != ending_color:
         for i in range(len(current)):
             if current[i] < ending_color[i]:
                 current[i] += 1
-            else: continue
-        screen.fill(tuple(current))
-        pygame.display.update()
-        pygame.time.delay(2)
-
-def quit():
-    pygame.quit()
-    pygame.font.quit()
-    pygame.display.quit()

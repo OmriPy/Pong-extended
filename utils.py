024f173225d85224540541ea90dc36ebff31f45f
Omri <110406612+OmriPy@users.noreply.github.com>
2022-09-22 20:25:59 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-from multipledispatch import dispatch
+from typing import overload
 
 class Button:
 
@@ -14,30 +14,14 @@ class Button:
         if clicked_pos[0] >= self.rect.x and clicked_pos[0] <= self.rect.right and\
             clicked_pos[1] >= self.rect.y and clicked_pos[1] <= self.rect.bottom:
             return True
-        else: return False
+        return False
 
-    @dispatch(tuple, tuple, bool)
-    def Draw(self, Button_color: tuple, Text_color: tuple, update: bool = True):
+    def Draw(self, Rect_color: tuple, Text_color: tuple, bg: tuple = 0, width: int = 0, border_radius: int = -1):
         text = self.font.render(self.text, 1, Text_color)
-        pygame.draw.rect(self.screen, Button_color, self.rect)
+        if bg != 0:
+            pygame.draw.rect(self.screen, bg, pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height), 0, border_radius)
+        pygame.draw.rect(self.screen, Rect_color, self.rect, width, border_radius)
         self.screen.blit(text, (self.rect.centerx - text.get_width() / 2, self.rect.centery - text.get_height() / 2))
-        if update: pygame.display.update()
-
-    @dispatch(tuple, tuple, tuple, int, bool)
-    def Draw(self, Outline_color: tuple, Text_color: tuple, bg: tuple, width: int, update: bool = True):
-        text = self.font.render(self.text, 1, Text_color)
-        pygame.draw.rect(self.screen, bg, pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height), 0)
-        pygame.draw.rect(self.screen, Outline_color, self.rect, width)
-        self.screen.blit(text, (self.rect.centerx - text.get_width() / 2, self.rect.centery - text.get_height() / 2))
-        if update: pygame.display.update()
-
-    @dispatch(tuple, tuple, tuple, int, int, bool)
-    def Draw(self, Outline_color: tuple, Text_color: tuple, bg: tuple, width: int, border_radius: int, update: bool = True):
-        text = self.font.render(self.text, 1, Text_color)
-        pygame.draw.rect(self.screen, bg, pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height), 0, border_radius)
-        pygame.draw.rect(self.screen, Outline_color, self.rect, width, border_radius)
-        self.screen.blit(text, (self.rect.centerx - text.get_width() / 2, self.rect.centery - text.get_height() / 2))
-        if update: pygame.display.update()
 
 def quit():
     pygame.display.quit()
